5c3a0354c13c53d7c9eb05086984e27740071446
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-07-18 22:30:31 +0700

    Init

---
+++ EntityPojo/src/main/java/com/dev/entitypojo/EntityPojoApplication.java
@@ -0,0 +1,13 @@
+package com.dev.entitypojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityPojoApplication.class, args);
+    }
+
+}
---
+++ EntityPojo/src/test/java/com/dev/entitypojo/EntityPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.entitypojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
