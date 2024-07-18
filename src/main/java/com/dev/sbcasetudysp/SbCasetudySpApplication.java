50a36b299ef3a19cbb9bdbde32d9e00a176124f3
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-07-18 21:25:01 +0700

    Init

---
+++ src/main/java/com/dev/sbcasetudysp/SbCasetudySpApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sbcasetudysp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCasetudySpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCasetudySpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/sbcasetudysp/SbCasetudySpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sbcasetudysp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCasetudySpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
