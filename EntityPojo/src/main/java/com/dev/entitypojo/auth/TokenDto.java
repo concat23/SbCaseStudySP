748a16e2d2127a86092ccfcffc4ac036796e4897
bang-dev <ecosystemprogramminglanguage@gmail.com>
2024-07-18 22:46:26 +0700

    Add entity class

---
+++ EntityPojo/src/main/java/com/dev/entitypojo/auth/TokenDto.java
@@ -0,0 +1,16 @@
+package com.dev.entitypojo.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+    private String grantType;
+    private String accessToken;
+}
+
---
+++ EntityPojo/src/main/java/com/dev/entitypojo/member/Authority.java
@@ -0,0 +1,5 @@
+package com.dev.entitypojo.member;
+
+public enum Authority {
+    ROLE_USER, ROLE_ADMIN
+}
